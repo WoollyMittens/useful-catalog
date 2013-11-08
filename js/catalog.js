d6317741322b26e1c7fca18bb57a2324a35ab55a
Maurice van Creij <maurice.vancreij@webqem.com>
2013-11-08 17:16:58 +1100

    Removed reference to useful.interactions

--- js/catalog.js
+++ js/catalog.js
@@ -3,7 +3,6 @@
 
 	@codekit-append "../src/useful.catalog.js"
 	@codekit-append "../../useful-gestures/src/useful.gestures.js"
-	@codekit-append "../../useful-interactions/src/useful.interactions.js"
 	@codekit-append "../../useful-transitions/src/useful.transitions.js"
 	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
 */
