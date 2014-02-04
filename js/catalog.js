1027c493dcea511577a37cb58e3ff4aa95621646
Maurice van Creij <maurice.vancreij@webqem.com>
2014-02-05 08:14:29 +1100

    Switched to grunt.js for pre-processing.

--- js/catalog.js
+++
@@ -1,4 +0,0 @@
-//@codekit-append "../src/useful.catalog.js"
-//@codekit-append "../../useful-gestures/src/useful.gestures.js"
-//@codekit-append "../../useful-transitions/src/useful.transitions.js"
-//@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
